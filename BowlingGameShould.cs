a47aed5d45a8eda934953fdb6b3c0726d2b8f413
timmygilissen <timmy.gilissen@gmail.com>
2016-11-21 19:54:31 +0100

    bowling kata

---
+++ BowlingGameShould.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+
+namespace bowlingKata
+{
+    public class BowlingGameShould
+    {
+        private Game _game;
+
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [Fact]
+        public void ReturnZeroWithAGutterGame()
+        {
+            Setup();
+            const int rolls = 20;
+            const int pins = 0;
+
+            RolMany(rolls,pins);
+
+            Assert.Equal(_game.Score(),0);
+        }
+
+        [Fact]
+        public void ReturnTwentyWhenAllThrowsAreOne()
+        {
+            Setup();
+            const int rolls = 20;
+            const int pins = 1;
+
+            RolMany(rolls,pins);
+
+            Assert.Equal(_game.Score(),20);
+        }
+
+        [Fact]
+        public void ReturnSixteenWithOneSpare()
+        {
+            Setup();
+
+            ThrowSpare();
+            _game.Roll(3);
+            RolMany(17, 0);
+
+            Assert.Equal(_game.Score(), 16);
+        }
+
+        [Fact]
+        public void ReturnTwentyFourWithOnStrike()
+        {
+            Setup();
+
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+           RolMany(16,0);
+
+            Assert.Equal(24,_game.Score());
+        }
+
+        [Fact]
+        public void ReturnThreeHonderWithAPerfectGame()
+        {
+            Setup();
+            RolMany(12,10);
+            Assert.Equal(300,_game.Score());
+        }
+
+        [Fact]
+        public void Return
+
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+
+        private void ThrowSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        private void RolMany(int rolls,int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,65 @@
+﻿namespace bowlingKata
+{
+    public class Game
+    {
+        private int _score;
+        private readonly int[] rolls = new int[21];
+        private int currentRoll;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            var roll = 0;
+
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrikeRole(roll))
+                {
+                    _score += 10 + StrikeBonus(roll);
+                    roll++;
+                }
+                else if (IsASpareFrame(roll))
+                {
+                    _score += 10 + SpareBonus(roll);
+                    roll += 2;
+                }
+                else
+                {
+                   _score += SumOffBallsInFrame(roll);
+                    roll += 2;
+                }
+            }
+
+            return _score;
+        }
+
+        private bool IsStrikeRole(int roll)
+        {
+            return rolls[roll] == 10;
+        }
+
+        private int SumOffBallsInFrame(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1];
+        }
+
+        private int StrikeBonus(int roll)
+        {
+            return rolls[roll + 1] + rolls[roll + 2];
+        }
+
+        private int SpareBonus(int roll)
+        {
+            return rolls[roll + 2];
+        }
+
+        private bool IsASpareFrame(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1] == 10;
+        }
+    }
+}
